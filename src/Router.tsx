717e7f7111d564f5467ef6a8e59d34d6bd0187b0
thereallucas98 <lucas1998david@gmail.com>
2022-11-16 19:38:25 -0300

    static pages

---
+++ src/Router.tsx
@@ -0,0 +1,14 @@
+import { Route, Routes } from 'react-router-dom'
+
+import { Checkout } from './pages/Checkout'
+import { Home } from './pages/Home'
+
+export function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/checkout" element={<Checkout />} />
+      <Route path="/success" element={<Checkout />} />
+    </Routes>
+  )
+}
---
+++ src/pages/Success/index.tsx
@@ -0,0 +1,7 @@
+export function Success() {
+  return (
+    <div>
+      <h1>Success Page</h1>
+    </div>
+  )
+}
